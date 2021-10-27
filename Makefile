8791aae7a3d7d4588cb425cdc59375e0a071eceb
net22sky <admin@i-22.ru>
2021-10-27 18:14:22 +0300

    start

---
+++ Makefile
@@ -0,0 +1,106 @@
+#-----------------------------------------------------------
+# Docker
+#-----------------------------------------------------------
+
+# Wake up docker containers
+up:
+	docker-compose up -d
+
+# Shut down docker containers
+down:
+	docker-compose down
+
+# Show a status of each container
+status:
+	docker-compose ps
+
+# Status alias
+s: status
+
+# Show logs of each container
+logs:
+	docker-compose logs
+
+# Restart all containers
+restart: down up
+
+
+# Build and up docker containers
+build:
+	docker-compose up -d --build
+
+# Build containers with no cache option
+build-no-cache:
+	docker-compose build --no-cache
+
+# Build and up docker containers
+rebuild: down build
+
+
+
+# Run terminal of the client container
+client:
+	docker-compose exec php_front /bin/sh
+
+
+
+
+
+
+#-----------------------------------------------------------
+# Database
+#-----------------------------------------------------------
+
+# Run database migrations
+db-migrate:
+	docker-compose exec php php artisan migrate
+
+# Migrate alias
+migrate: db-migrate
+
+# Run migrations rollback
+db-rollback:
+	docker-compose exec php php artisan migrate:rollback
+
+# Rollback alias
+rollback: db-rollback
+
+# Run seeders
+db-seed:
+	docker-compose exec php php artisan db:seed
+
+# Fresh all migrations
+db-fresh:
+	docker-compose exec php php artisan migrate:fresh
+
+# Dump database into file
+db-dump:
+	docker-compose exec postgres pg_dump -U app -d app > docker/postgres/dumps/dump.sql
+
+
+
+#-----------------------------------------------------------
+# Testing
+#-----------------------------------------------------------
+
+# Run phpunit tests
+test:
+	docker-compose exec php vendor/bin/phpunit --order-by=defects --stop-on-defect
+
+# Run all tests ignoring failures.
+test-all:
+	docker-compose exec php vendor/bin/phpunit --order-by=defects
+
+# Run phpunit tests with coverage
+coverage:
+	docker-compose exec php vendor/bin/phpunit --coverage-html tests/report
+
+# Run phpunit tests
+dusk:
+	docker-compose exec php php artisan dusk
+
+# Generate metrics
+metrics:
+	docker-compose exec php vendor/bin/phpmetrics --report-html=api/tests/metrics api/app
+
+
---
+++ webpack.dev.config.js
@@ -0,0 +1,149 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const minify = require('optimize-css-assets-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const autoprefixer = require('autoprefixer');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimaizeCssAccetPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const  {CleanWebpackPlugin}  = require('clean-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+ 
+module.exports = {
+    externals: {
+        jquery: 'jQuery'
+      },
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'scripts.js'
+    },
+    resolve: {
+        alias: {
+          vue: 'vue/dist/vue.js'
+        }
+      },
+	module: {
+		rules: [{
+            test:/\.(s*)js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+            },
+            /*test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loader: {
+                scss: 'vue-style-loader!css-loader!sass-loader'
+                }
+            },*/
+            {
+			//test:/\.(s*)css$/,
+                test: /\.scss$/,
+			/*use: [
+				miniCss.loader,
+                'css-loader',
+                'postcss-loader',
+				'sass-loader',
+            ]*/
+            use: [
+                miniCss.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                        config: {
+                            path: 'src/postcss.config.js'
+                        }
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                    
+                }
+            ],
+                include: path.join(__dirname, 'src'),
+            },
+        {
+            test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts'
+                }
+              }
+            ]
+          },
+          /*{
+            test: /\.svg$/,
+            loader: 'svg-inline-loader',
+           },*/
+          {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            loader: 'file-loader',
+            options: {
+                name: '[path][name].[ext]',
+                outputPath: 'images',
+            },
+          }
+
+    ]
+	},
+	optimization: {
+		minimizer: [
+            /*new UglifyJsPlugin({
+                test: /\.js(\?.*)?$/i,
+                extractComments: true,
+              }),
+
+             */
+            new TerserPlugin({
+                test: /\.js(\?.*)?$/i,
+                extractComments: true,
+                sourceMap: true,
+            }),
+			new minify({})
+        ],
+        splitChunks: {
+            // include all types of chunks
+            chunks: 'all'
+          },
+	},
+	plugins: [
+        //new DashboardPlugin(),
+        //new CleanWebpackPlugin(   ),
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map'
+        }),
+		new miniCss({
+			filename: '../dist/main.css',
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery/dist/jquery.min.js",
+            jQuery: "jquery/dist/jquery.min.js",
+            "window.jQuery": "jquery/dist/jquery.min.js"
+          }),
+        new LiveReloadPlugin(),
+        new  CleanWebpackPlugin ( ),
+    ]
+};
+
+
+
+            
+
